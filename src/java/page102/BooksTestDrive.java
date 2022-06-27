8b5fa34aebca1173e2f8f30b73a1088e20ab2519
Vasiliy Sidorenko <sidorenkovn@list.ru>
2022-06-27 22:45:08 +0300

    + pages

---
+++ src/java/page102/BooksTestDrive.java
@@ -0,0 +1,31 @@
+
+class BooksTestDrive
+{
+    public static void main(String[] args)
+    {
+        Books[] myBooks = new Books[3];
+        int x = 0;
+
+        while(x < myBooks.length)
+        {
+            myBooks[x] = new Books();
+            x++;
+        }
+
+        myBooks[0].title = "The Grapes of Java";
+        myBooks[1].title = "The Java Gatsby";
+        myBooks[2].title = "The Java Cookbook";
+        myBooks[0].author = "bob";
+        myBooks[1].author = "sue";
+        myBooks[2].author = "ian";
+
+        x = 0;
+        while(x < 3)
+        {
+            System.out.print(myBooks[x].title);
+            System.out.print(" by ");
+            System.out.println(myBooks[x].author);
+            x = x + 1;
+        }
+    }
+}
